5d48b6aa6909ebbce3c48672803f721ba546625b
tracy.teng <tyh11830@126.com>
2019-01-09 15:13:17 +0800

    project-vue finish

---
+++ build/webpack.config.practice.js
@@ -0,0 +1,105 @@
+const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.config.base.js')
+
+const defaultPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: '"development"'
+    }
+  }),
+  new HTMLPlugin({
+    template: path.join(__dirname, 'template.html')
+  })
+]
+
+const devServer = {
+    port: 8080,
+    host: '0.0.0.0',
+    overlay: {
+      errors: true,
+    },
+    hot: true
+  }
+let config
+
+config = merge(baseConfig, {
+    entry: path.join(__dirname, '../practice/index.js'),
+    devtool: '#cheap-module-eval-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.styl/,
+          use: ExtractPlugin.extract({
+            fallback: 'vue-style-loader',
+            use: [
+              'css-loader',
+              {
+                loader: 'postcss-loader',
+                options: {
+                  sourceMap: true,
+                }
+              },
+              'stylus-loader'
+            ]
+          })
+        }
+      ]
+    },
+    plugins: defaultPluginsconcat([
+      new ExtractPlugin('styles.[contentHash:8].css'),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor'
+      }),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'runtime'
+      }),
+    ])
+  })
+      app: path.join(__dirname, '../client/index.js'),
+      vendor: ['vue']
+    },
+    output: {
+      filename: '[name].[chunkhash:8].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.styl/,
+          use: ExtractPlugin.extract({
+            fallback: 'vue-style-loader',
+            use: [
+              'css-loader',
+              {
+                loader: 'postcss-loader',
+                options: {
+                  sourceMap: true,
+                }
+              },
+              'stylus-loader'
+            ]
+          })
+        }
+      ]
+    },
+    devServer,
+    //import Vue from 'vue'
+    resolve: {
+      alias: {
+        'vue': path.join(__dirname, '../node_modules/vue/dist/vue.esm.js')
+      }
+    },
+    plugins: defaultPluginsconcat([
+      new ExtractPlugin('styles.[contentHash:8].css'),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor'
+      }),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'runtime'
+      }),
+    ])
+  })
+
+module.exports = config
